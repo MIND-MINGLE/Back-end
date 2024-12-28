6cd29cc6ef6e3d1c6dd0d31d5159ceafbbec8127
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-12-29 00:06:52 +0700

    EPIC
    "Now this is PEAK" - Optimus Prime /* Oh oah ack ack */

--- MindMingle/Domain/Entity/Account.cs
+++ MindMingle/Domain/Entity/Account.cs
@@ -9,7 +9,7 @@ namespace Domain.Entity
         required public string AccountId { get; set; }
         required public string AccountName { get; set; }
 		required public string Password { get; set; }
-		required public int RoleId { get; set; }
+		required public string RoleId { get; set; }
 		required public string Email { get; set; }
 		public string? Avatar { get; set; }
 		public DateTime LastLogin { get; set; }
---
+++ MindMingle/Infrastructure/MMDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure
+{
+    public class MMDbContextFactory : IDesignTimeDbContextFactory<MMDbContext>
+    {
+        public MMDbContext CreateDbContext(string[] args)
+        {
+            // Build configuration to read the connection string from appsettings.json
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()).FullName)  // One level up to the solution folder
+                 .AddJsonFile("API/appsettings.json", optional: false, reloadOnChange: true) // Accessing appsettings.json from the 'API' folder
+                 .Build();
+
+            // Configure DbContextOptions with the connection string
+            var optionsBuilder = new DbContextOptionsBuilder<MMDbContext>();
+            optionsBuilder.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+                new MySqlServerVersion(new Version(8, 0, 30)));
+
+            return new MMDbContext(optionsBuilder.Options);
+        }
+    }
+}
+
