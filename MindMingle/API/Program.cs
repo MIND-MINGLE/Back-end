07c87a3a47230e1730b16e10ca931ffb913bc998
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2025-02-13 14:38:36 +0700

    Update Program.cs

--- MindMingle/API/Program.cs
+++ MindMingle/API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("*")
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "https://mindmingle202.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
