6b0fdb4d331da1756ec35d70540182e1ff658c7a
drekhai123 <khai.lumberjack@gmail.com>
2025-03-13 14:06:50 +0700

    PatientResponse
    Create PatientResponse and get PatientResponseBySurveyId(paging apply)

---
+++ MindMingle/Application/Response/PatientResponse/PatientResResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Response.PatientResponse
+{
+    public class PatientResResponse
+    {
+        public string ResponseId { get; set; }
+        public string SurveyId { get; set; }
+        public string QuestionId { get; set; }
+        public string? AnswerId { get; set; }
+        public string? CustomerAnswer { get; set; }
+        public int? Score { get; set; }
+    }
+}
---
+++ MindMingle/Infrastructure/Repository/PatientResponseRepository.cs
@@ -0,0 +1,32 @@
+﻿using Application.IRepository;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class PatientResponseRepository : GenericRepository<PatientResponse>, IPatientResponseRepository
+    {
+        private readonly MMDbContext _mmDbContext;
+
+        public PatientResponseRepository(MMDbContext mmDbContext) : base(mmDbContext)
+        {
+            _mmDbContext = mmDbContext;
+        }
+
+        public async Task<List<PatientResponse>> GetResponsesBySurveyIdAsync(string surveyId, int pageIndex = 1, int pageSize = 10)
+        {
+            return await _mmDbContext.PatientResponses
+                .Where(pr => pr.PatientSurveyId == surveyId)
+                .Include(pr => pr.Question)
+                .Include(pr => pr.Answer)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
