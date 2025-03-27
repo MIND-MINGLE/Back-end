533e0d2efc01e5e03cbd459f3c6f7a58085794eb
Hendrix Blaze <blazehendrix007@gmail.com>
2025-03-27 16:13:47 +0700

    k

---
+++ MindMingle/Infrastructure/Migrations/20250327091140_rating.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class rating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Score",
+                table: "Ratings",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentUrl",
+                table: "Payments",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Score",
+                table: "Ratings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.UpdateData(
+                table: "Payments",
+                keyColumn: "PaymentUrl",
+                keyValue: null,
+                column: "PaymentUrl",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentUrl",
+                table: "Payments",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
