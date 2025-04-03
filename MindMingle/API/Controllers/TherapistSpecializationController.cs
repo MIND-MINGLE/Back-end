8a0ba98000857d77c20b291b68831f7250c6e817
Vũ Đức Anh <ducanhvu43@gmail.com>
2025-04-04 00:00:23 +0700

    Update TherapistSpecializationController.cs

--- MindMingle/API/Controllers/TherapistSpecializationController.cs
+++ MindMingle/API/Controllers/TherapistSpecializationController.cs
@@ -38,9 +38,9 @@ namespace API.Controllers
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTherapistSpecializationByIdAsync(string id)
+        public async Task<IActionResult> DeleteTherapistSpecializationByIdAsync(string theraSpecId, string specId)
         {
-            var response = await _therapistSpecializationService.DeleteTherapistSpecializationByIdAsync(id);
+            var response = await _therapistSpecializationService.DeleteTherapistSpecializationByIdAsync(theraSpecId,specId);
             return response.IsSuccess ? Ok(response) : BadRequest(response);
         }
     }
