e1e3bbbb626e7584aba29ceacec0a8f519566eb4
Hendrix Blaze <blazehendrix007@gmail.com>
2025-04-09 15:13:56 +0700

    Google Login API

---
+++ MindMingle/Application/Request/Account/GoogleLoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Request.Account
+{
+	public class GoogleLoginRequest
+	{
+		public string Email { get; set; } = string.Empty;
+		public bool Email_verified { get; set; } = false;
+        public string Family_name { get; set; } = string.Empty;
+        public string Given_name { get; set; } = string.Empty;
+        public string Picture { get; set; } = string.Empty;
+        public string Sub { get; set; } = string.Empty;
+        public string RoleId { get; set; } = string.Empty;
+    }
+}
--- MindMingle/Application/Services/AuthService.cs
+++ MindMingle/Application/Services/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Interface;
 using Application.Request.Account;
+using Application.Request.Role;
 using Application.Response;
 using AutoMapper;
 using Domain;
@@ -7,37 +8,37 @@ using Domain.Entity;
 using Microsoft.AspNetCore.Identity; // PasswordHasher<Account>
 
 namespace Application.Services
-{ 
-	public class AuthService : IAuthService
-	{
-		private IUnitOfWorks _unitOfWorks;
-		private AppSetting _appSettings;
-		//private IClaimService _claimService;
-		private IEmailService _emailService;
-		private ITokenService _tokenService;
-		private IPasswordHasher<Account> _passwordHasher;
+{
+    public class AuthService : IAuthService
+    {
+        private IUnitOfWorks _unitOfWorks;
+        private AppSetting _appSettings;
+        //private IClaimService _claimService;
+        private IEmailService _emailService;
+        private ITokenService _tokenService;
+        private IPasswordHasher<Account> _passwordHasher;
 
-		public AuthService(IUnitOfWorks unitOfWorks, AppSetting appSettings, IPasswordHasher<Account> passwordHasher, IEmailService emailService, ITokenService tokenService)
-		{
-			_unitOfWorks = unitOfWorks;
-			_appSettings = appSettings;
-			_passwordHasher = passwordHasher;
-			_emailService = emailService;	
-			_tokenService = tokenService;
-		}
+        public AuthService(IUnitOfWorks unitOfWorks, AppSetting appSettings, IPasswordHasher<Account> passwordHasher, IEmailService emailService, ITokenService tokenService)
+        {
+            _unitOfWorks = unitOfWorks;
+            _appSettings = appSettings;
+            _passwordHasher = passwordHasher;
+            _emailService = emailService;
+            _tokenService = tokenService;
+        }
         public async Task<ApiResponse> ActivateAccountAsync(string accountId)
-		{
+        {
             ApiResponse response = new ApiResponse();
             try
             {
                 var account = await _unitOfWorks.AccountRepo.GetAsync(x => x.AccountId == accountId);
-				if (account == null)
-				{
+                if (account == null)
+                {
                     return response.SetBadRequest(message: "User not found!!");
                 }
-				else
-				{
-					await _unitOfWorks.AccountRepo.UpdateFieldAsync(accountId, a => a.IsDisabled, false);
+                else
+                {
+                    await _unitOfWorks.AccountRepo.UpdateFieldAsync(accountId, a => a.IsDisabled, false);
                     return response.SetOk($"Account {accountId} is activated");
                 }
             }
@@ -47,158 +48,253 @@ namespace Application.Services
 
             }
         }
+        public async Task<ApiResponse> GoogleLoginAsync(GoogleLoginRequest request)
+        {
+            ApiResponse response = new ApiResponse();
+            var user = await _unitOfWorks.AccountRepo.GetAsync(x => x.Email == request.Email);
+            if (user != null)
+            {
+                if (user.IsDisabled)
+                {
+                    response.SetBadRequest(message: "Account Is Not Verified");
+                    return response;
+                }
+                user.LastLogin = DateTime.Now;
+                await _unitOfWorks.SaveChangeAsync();
+
+                //var claims =  _claimService.GetClaim();
+                var token = _tokenService.CreateToken(user);
+                return response.SetOk(token);
+            }
+            else
+            {
+                var existedUser = await _unitOfWorks.AccountRepo.GetAsync(x => x.Email == request.Email);
+                if (existedUser != null)
+                {
+                    response.SetBadRequest(message: "The Email/Account Name is already existed !");
+                    return response;
+                }
+                else
+                {
+                    Account newAccount = new Account()
+                    {
+                        RoleId = request.RoleId,
+                        AccountId = request.Sub,
+                        Email = request.Email,
+                        AccountName = request.Family_name + "_" + request.Given_name,
+                        Password = request.Sub,
+                        IsDisabled = request.RoleId.Equals("doc") || request.RoleId.Equals("agent"),
+                        //Disable business account on default, admin verified
+                        Avatar = request.Picture,
+                        IsEmailVerified = request.Email_verified
+                    };
+                    await _unitOfWorks.AccountRepo.AddAsync(newAccount);
+                    await _unitOfWorks.SaveChangeAsync();
+                    
+                    switch (request.RoleId)
+                    {
+                        case "seeker": {
+                                Patient newPatient = new Patient() {
+                                    AccountId = request.Sub,
+                                    FirstName = request.Given_name,
+                                    LastName = request.Family_name,
+                                    Dob = DateTime.Now,
+                                    Gender = "Unknown",
+                                    PatientId = Guid.NewGuid().ToString(),
+                                    PhoneNumber = "Unknown",
+                                    CreatedAt = DateTime.Now,
+                                    UpdatedAt = DateTime.Now
+                                };
+                                await _unitOfWorks.PatientRepo.AddAsync(newPatient);
+                                await _unitOfWorks.SaveChangeAsync();
+                                break;
+                            }
+                        case "doc": {
+                                Therapist newTherapist = new Therapist() {
+                                    AccountId = request.Sub,
+                                    FirstName = request.Given_name,
+                                    LastName = request.Family_name,
+                                    Dob = DateTime.Now,
+                                    Gender = "Unknown",
+                                    TherapistId = Guid.NewGuid().ToString(),
+                                    PhoneNumber = "Unknown",
+                                    CreatedAt = DateTime.Now,
+                                    UpdatedAt = DateTime.Now,
+                                    PricePerHour = 500000
+                                };
+                                await _unitOfWorks.TherapistRepo.AddAsync(newTherapist);
+                                await _unitOfWorks.SaveChangeAsync();
+                                break; }
+                        case "agent": {
+                                //TODO Add Agent
+                                break; }
+                        default:
+                            {
+                                response.SetBadRequest(message: "Role Not Found!");
+                                return response;
+                            }
+                    }
+                    var token = _tokenService.CreateToken(newAccount);
+                    return response.SetOk(token);
+                   
+                }
+
+            }
+
+        }
         public async Task<ApiResponse> LoginAsync(LoginRequest request)
-		{
-			ApiResponse response = new ApiResponse();
-			var user = await _unitOfWorks.AccountRepo.GetAsync(x => x.Email == request.EmailOrAccountName || x.AccountName == request.EmailOrAccountName);
-			if (user == null)
-			{
-				response.SetBadRequest(message: "Invalid AccountName, Email or Password");
-				return response;
-			}
-			else if (user.IsDisabled)
-			{
+        {
+            ApiResponse response = new ApiResponse();
+            var user = await _unitOfWorks.AccountRepo.GetAsync(x => x.Email == request.EmailOrAccountName || x.AccountName == request.EmailOrAccountName);
+            if (user == null)
+            {
+                response.SetBadRequest(message: "Invalid AccountName, Email or Password");
+                return response;
+            }
+            else if (user.IsDisabled)
+            {
                 response.SetBadRequest(message: "Account Is Not Verified");
                 return response;
             }
-			var passwordVerificationResult = _passwordHasher.VerifyHashedPassword(user, user.Password, request.Password);
-			if (passwordVerificationResult != PasswordVerificationResult.Success)
-			{
-				response.SetBadRequest(message: "Invalid Password");
-				return response;
-			}
+            var passwordVerificationResult = _passwordHasher.VerifyHashedPassword(user, user.Password, request.Password);
+            if (passwordVerificationResult != PasswordVerificationResult.Success)
+            {
+                response.SetBadRequest(message: "Invalid Password");
+                return response;
+            }
 
-			user.LastLogin = DateTime.Now;
+            user.LastLogin = DateTime.Now;
             await _unitOfWorks.SaveChangeAsync();
 
             //var claims =  _claimService.GetClaim();
             var token = _tokenService.CreateToken(user);
-			response.SetOk(token);
-			return response;
-		}
+            response.SetOk(token);
+            return response;
+        }
 
-		public async Task<ApiResponse> VerifyEmailAsync(string accountId, string verificationCode)
-		{
-			ApiResponse response = new ApiResponse();
+        public async Task<ApiResponse> VerifyEmailAsync(string accountId, string verificationCode)
+        {
+            ApiResponse response = new ApiResponse();
 
-			//Retrieve the verification code 
-			var verified = await _unitOfWorks.EmailVerificationRepo.GetAsync(x => x.AccountId == accountId && x.VerificationCode == verificationCode && x.IsUsed == false);
+            //Retrieve the verification code 
+            var verified = await _unitOfWorks.EmailVerificationRepo.GetAsync(x => x.AccountId == accountId && x.VerificationCode == verificationCode && x.IsUsed == false);
 
-			//Verification record not found
-			if(verified == null)
-			{
-				response.SetBadRequest(message: "Invalid Code!!");
-				return response;
-			}
+            //Verification record not found
+            if (verified == null)
+            {
+                response.SetBadRequest(message: "Invalid Code!!");
+                return response;
+            }
 
 
-			//Check if the code has expired
-			if(verified.ExpiresAt < DateTime.Now)
-			{
-				response.SetBadRequest(message: "The verification code has expired!!");
-				return response; 
-			}
+            //Check if the code has expired
+            if (verified.ExpiresAt < DateTime.Now)
+            {
+                response.SetBadRequest(message: "The verification code has expired!!");
+                return response;
+            }
 
-			//Mark the verification code as verified
-			verified.IsUsed = true;
-			await _unitOfWorks.SaveChangeAsync();
+            //Mark the verification code as verified
+            verified.IsUsed = true;
+            await _unitOfWorks.SaveChangeAsync();
 
-			//Mark the user's email as verified 
-			var user = await _unitOfWorks.AccountRepo.GetAsync(x => x.AccountId == accountId);
-			if(user == null)
-			{
-				response.SetBadRequest(message: "User not found!!");
-				return response;
-			}
+            //Mark the user's email as verified 
+            var user = await _unitOfWorks.AccountRepo.GetAsync(x => x.AccountId == accountId);
+            if (user == null)
+            {
+                response.SetBadRequest(message: "User not found!!");
+                return response;
+            }
 
-			user.IsEmailVerified = true;
-			await _unitOfWorks.SaveChangeAsync();
+            user.IsEmailVerified = true;
+            await _unitOfWorks.SaveChangeAsync();
 
-			response.SetOk("Email verified successfully!!");
-			return response;
-		}
+            response.SetOk("Email verified successfully!!");
+            return response;
+        }
 
-		public async Task<ApiResponse> RegisterAsync(UserRegisterRequest userRequest,string roleId)
-		{
-			ApiResponse response = new ApiResponse();
-			try
-			{
-				var checkPassword = CheckUserPassword(userRequest);
-				if (!checkPassword){
-					response.SetBadRequest(message: "Confirm password is wrong !");
-					return response;
-				}
-				var existedUser = await _unitOfWorks.AccountRepo.GetAsync(x => x.Email == userRequest.Email || x.AccountName == userRequest.AccountName);
-				if(existedUser != null)
-				{
-					response.SetBadRequest(message: "The Email/Account Name is already existed !");
-					return response;
-				}
+        public async Task<ApiResponse> RegisterAsync(UserRegisterRequest userRequest, string roleId)
+        {
+            ApiResponse response = new ApiResponse();
+            try
+            {
+                var checkPassword = CheckUserPassword(userRequest);
+                if (!checkPassword)
+                {
+                    response.SetBadRequest(message: "Confirm password is wrong !");
+                    return response;
+                }
+                var existedUser = await _unitOfWorks.AccountRepo.GetAsync(x => x.Email == userRequest.Email || x.AccountName == userRequest.AccountName);
+                if (existedUser != null)
+                {
+                    response.SetBadRequest(message: "The Email/Account Name is already existed !");
+                    return response;
+                }
 
-				Account newAccount = new Account()
-				{
-					RoleId = roleId,
-					AccountId = Guid.NewGuid().ToString(),
-					Email = userRequest.Email,
-					AccountName = userRequest.AccountName,
-					Password = userRequest.Password,
-					IsDisabled = roleId.Equals("doc")||roleId.Equals("agent")
-					//Disable business account on default, admin verified
+                Account newAccount = new Account()
+                {
+                    RoleId = roleId,
+                    AccountId = Guid.NewGuid().ToString(),
+                    Email = userRequest.Email,
+                    AccountName = userRequest.AccountName,
+                    Password = userRequest.Password,
+                    IsDisabled = roleId.Equals("doc") || roleId.Equals("agent")
+                    //Disable business account on default, admin verified
                 };
 
-				//Hash the password
-				var hashedPassword = _passwordHasher.HashPassword(newAccount, userRequest.Password);
-				newAccount.Password = hashedPassword;
+                //Hash the password
+                var hashedPassword = _passwordHasher.HashPassword(newAccount, userRequest.Password);
+                newAccount.Password = hashedPassword;
 
-				await _unitOfWorks.AccountRepo.AddAsync(newAccount);
-				await _unitOfWorks.SaveChangeAsync();
+                await _unitOfWorks.AccountRepo.AddAsync(newAccount);
+                await _unitOfWorks.SaveChangeAsync();
 
-				//Generate verification code 
-				var verificationCode = GenerateVerificationCode();
-				EmailVerification emailVerification = new EmailVerification()
-				{
-					VerificationId = Guid.NewGuid().ToString(),
-					AccountId = newAccount.AccountId,
-					VerificationCode = verificationCode,
-					ExpiresAt = DateTime.Now.AddMinutes(10), //code valid for 10 minutes
-					IsUsed = false
-				};
+                //Generate verification code 
+                var verificationCode = GenerateVerificationCode();
+                EmailVerification emailVerification = new EmailVerification()
+                {
+                    VerificationId = Guid.NewGuid().ToString(),
+                    AccountId = newAccount.AccountId,
+                    VerificationCode = verificationCode,
+                    ExpiresAt = DateTime.Now.AddMinutes(10), //code valid for 10 minutes
+                    IsUsed = false
+                };
 
-				await _unitOfWorks.EmailVerificationRepo.AddAsync(emailVerification);
-				await _unitOfWorks.SaveChangeAsync();
+                await _unitOfWorks.EmailVerificationRepo.AddAsync(emailVerification);
+                await _unitOfWorks.SaveChangeAsync();
 
-				//Prepare email content
-				string emailContent = $"Dear {newAccount.AccountName},<br/>Please use the following verification code to validate your email: <strong>{verificationCode}</strong>.<br/>The code will expire in 10 minutes.";
-
-
-				//Send validation email
-				var emailResponse = await _emailService.SendValidationEmail(newAccount.Email, emailContent);
-				if (!emailResponse.IsSuccess)
-				{
-					response.SetBadRequest(message: "Failed to send verification email !!");
-					return response;
-				}
-
-				response.SetOk(newAccount.AccountId);
-				return response;
-			}
-			catch (Exception ex)
-			{
-				return response.SetBadRequest($"Error: {ex.Message}. Details: {ex.InnerException?.Message}");
-			}
-		}
+                //Prepare email content
+                string emailContent = $"Dear {newAccount.AccountName},<br/>Please use the following verification code to validate your email: <strong>{verificationCode}</strong>.<br/>The code will expire in 10 minutes.";
 
 
-		private string GenerateVerificationCode()
-		{
-			Random random = new Random();
-			return random.Next(100000, 999999).ToString(); // Generate a 6-digit code
-		}
-		private bool CheckUserPassword(UserRegisterRequest user)
-		{
-			if (user.Password is null) return false;
-			return (user.Password.Equals(user.ConfirmPassword));
-		}
-	}
+                //Send validation email
+                var emailResponse = await _emailService.SendValidationEmail(newAccount.Email, emailContent);
+                if (!emailResponse.IsSuccess)
+                {
+                    response.SetBadRequest(message: "Failed to send verification email !!");
+                    return response;
+                }
+
+                response.SetOk(newAccount.AccountId);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                return response.SetBadRequest($"Error: {ex.Message}. Details: {ex.InnerException?.Message}");
+            }
+        }
+
+
+        private string GenerateVerificationCode()
+        {
+            Random random = new Random();
+            return random.Next(100000, 999999).ToString(); // Generate a 6-digit code
+        }
+        private bool CheckUserPassword(UserRegisterRequest user)
+        {
+            if (user.Password is null) return false;
+            return (user.Password.Equals(user.ConfirmPassword));
+        }
+    }
 
 }
